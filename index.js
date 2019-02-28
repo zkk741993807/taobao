47773a3ff327114326748a60fcb8fb150341522a
zkk741993807 <741993807@qq.com>
2019-02-28 18:41:19 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+// /*********** container-center-top-flash******************************/ 
+// var topFlashCn=document.getElementById("container-center-top-flash");
+// var topFlashUl=document.getElementById("container-center-top-flash-pic");
+// function move(obj,n){
+//     obj.timer=setInterval(function(){
+//         var off=obj.offsetLeft;
+//         if(492*n-Math.abs(off)<3){
+//             obj.style.left=-492*n+"px";
+//             clearInterval(obj.timer);
+//         }else{
+//             obj.style.left=off-40+"px";
+//         }
+       
+//     },30)
+// }
+// var n=0;
+
+
+// setInterval(function(){
+//     var off=topFlashUl.offsetLeft;
+//     if(Math.abs(off)>=2460){
+//         topFlashUl.style.left="0px";
+//         n=0;
+//     }else{
+//         n++;
+//         move(topFlashUl,n)
+//     }
+    
+// },1000)
